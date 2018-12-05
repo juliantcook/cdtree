ffa7e0bf383b180316c3f378ef3aa73ae129bc86
Julian Cook <juliantcook@gmail.com>
2018-12-05 22:27:41 +0000

    fix test

--- spec/draw-file-tree.spec.js
+++ spec/draw-file-tree.spec.js
@@ -1,10 +1,21 @@
 describe("draw", () => {
     const draw = require('../src/draw-file-tree');
     const File = require('../src/file');
+    const clc = require('cli-color');
 
-    // TODO how to test text colour?
+    const testDir = 'spec/test.d';
+
     it("should draw a directory with nothing selected", () => {
-        const file = new File('spec/test.d');
-        expect(draw(file)).toEqual('spec/test.d\n');
+        const file = new File(testDir);
+        expect(draw(file)).toEqual(`${clc.cyan(testDir)}\n`);
+    });
+
+    it('should draw a directory with a single folder inside', () => {
+        const file = new File(testDir);
+        file.selectChild();
+        expect(draw(file)).toEqual(
+            `${clc.cyan(testDir)}\n` +
+            `├─ ${clc.cyan.black.bgYellow('single-file.d')}\n`
+        );
     });
 });
